f10fe817b66b6e21a84e7586065c7643f86e9b6a
Gaurav0824 <gauravgupta1999kr@gmail.com>
2020-04-06 11:12:44 +0530

    c++

--- Start.cpp
+++ Start.cpp
@@ -1,14 +1,78 @@
 #include<iostream>
-#include<regex>
+#include<fstream>
+#include<stdio.h>
+#include<string.h>
+
+// #include<iostream>
+// #include<regex>
+// #include<dirent.h>
+// #include<dir.h>
+// #include<direct.h>
+// #include<process.h>
+// #include<dos.h>
+
+// #include <iostream>
+// #include <fstream>
+// #include <cstring>
+// #include <string.h>
+// #include <iomanip>
+// #include <cstdio>
+// #include <conio.h>
+// #include <ctime>
+// #include <stdlib.h>
+// #include <bits/stdc++.h>
+
+// #include<windows.h>
 
 using namespace std;
 
+int i=0;
+
+// char files[50][30];
+// void directory();
+// void directory()
+// { 
+//     // const char str[10]=".";
+//     // DIR *dir;
+//     // struct dirent *ent;
+
+//     // if ((dir = opendir (str)) != NULL)
+//     // {
+//     //     // print all the files and directories within directory
+//     //     while ((ent = readdir (dir)) != NULL)
+//     //     {
+//     //         sprintf(files[i++],"%s \\ ",ent->d_name); 
+//     //         //printf("%s \n",ent->d_name);  
+//     //     }
+//     //     closedir (dir);
+//     // }
+//     // else
+//     // {
+//     //     cout<<"could not open directory";
+//     //     perror ("");
+//     // }
+
+//     getch();
+// }
+
 int main()
 {
-    const char *first = "abc";
-    regex rx("a(b)c");
-    
-    bool found = regex_match(first,rx);
-    if (found)
-        wcout << L"Regex found in abc" << endl;
+    //directory();
+    char line[50]; 
+    char cmd[70];
+    system("DIR .\\*.jpg /B > file.txt");
+    fstream file;
+    file.open("fil.txt",ios::in);
+    // while(file)
+    // {   
+        strcpy(cmd," python test_image.py  ");
+        file>>line;
+        //cout<<line<<endl;
+        strcat(cmd,line);
+        cout<<cmd<<endl;
+        system(cmd);
+    // }
+    file.close();
+
 }
+
